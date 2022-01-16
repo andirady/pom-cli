a0c5fc74d8bb337f2eba801504b685428e39f44e
andirady <andirady@kendra-solutions.com>
2022-01-16 18:38:44 +0800

    Forgot to update test classes

--- src/test/java/com/github/andirady/pomcli/SearchCommandTest.java
+++ src/test/java/com/github/andirady/pomcli/SearchCommandTest.java
@@ -5,13 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-class SearchTest {
+class SearchCommandTest {
 
 	@Test
 	@Disabled
 	void test() {
-		var search = new Search();
-		search.arg = new Search.Exclusive();
+		var search = new SearchCommand();
+		search.arg = new SearchCommand.Exclusive();
 		search.arg.c = "Unchecked";
 		assertDoesNotThrow(search::run);
 	}
