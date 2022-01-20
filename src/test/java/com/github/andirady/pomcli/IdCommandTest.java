f7f0c16cbc3fb2239a0f93ac502760d9c71b4958
andirady <andirady@kendra-solutions.com>
2022-01-20 18:38:29 +0800

    Use Jimfs instead of tempdir

--- src/test/java/com/github/andirady/pomcli/IdCommandTest.java
+++ src/test/java/com/github/andirady/pomcli/IdCommandTest.java
@@ -7,26 +7,35 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.FileSystem;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.google.common.jimfs.Jimfs;
+
+import picocli.CommandLine.Model.CommandSpec;
+
 class IdCommandTest {
 
-    private Path tempDir;
+
+    IdCommand cmd;
+    CommandSpec spec;
+    FileSystem fs;
 
     @BeforeEach
     void setup() throws IOException {
-        tempDir = Files.createTempDirectory("pomcli-test");
+        fs = Jimfs.newFileSystem();
+        cmd = new IdCommand();
+        spec = CommandSpec.forAnnotatedObject(cmd);
     }
 
     @Test
     void shouldCreateNewFileIfPomNotExists() {
-        var pomPath = tempDir.resolve("pom.xml");
+        var pomPath = fs.getPath("pom.xml");
         var projectId = "com.example:my-app:0.0.1";
 
-        var cmd = new IdCommand();
         cmd.pomPath = pomPath;
         cmd.id = projectId;
         cmd.run();
@@ -35,10 +44,9 @@ class IdCommandTest {
 
     @Test
     void shouldAutomaticallySetVersionIfNotSpecified() throws Exception {
-        var pomPath = tempDir.resolve("pom.xml");
+        var pomPath = fs.getPath("pom.xml");
         var projectId = "com.example:my-app";
 
-        var cmd = new IdCommand();
         cmd.pomPath = pomPath;
         cmd.id = projectId;
         cmd.run();
@@ -47,11 +55,10 @@ class IdCommandTest {
     
     @Test
     void shouldSetPackaging() throws Exception {
-        var pomPath = tempDir.resolve("pom.xml");
+        var pomPath = fs.getPath("pom.xml");
         var projectId = "com.example:my-app:0.0.1";
         var packaging = "war";
 
-        var cmd = new IdCommand();
         cmd.pomPath = pomPath;
         cmd.id = projectId;
         cmd.as = packaging;
@@ -61,29 +68,33 @@ class IdCommandTest {
     
     @Test
     void shouldSetGroupIdToUnnamedIfNotSpeficied() throws Exception {
-        var pomPath = tempDir.resolve("pom.xml");
-        var projectId = "my-app";
-
-        var cmd = new IdCommand();
-        cmd.pomPath = pomPath;
-        cmd.id = projectId;
-        cmd.run();
+        var cmd = newUnnamed();
         assertEquals("jar unnamed:my-app:0.0.1-SNAPSHOT", cmd.readProjectId());    
     }
     
     @Test
     void shouldUpdateExistingPom() throws Exception {
-        var pomPath = tempDir.resolve("pom.xml");
+        newUnnamed();
+
+        var pomPath = fs.getPath("pom.xml");
         var projectId = "com.example:my-app";
 
-        shouldSetGroupIdToUnnamedIfNotSpeficied();
-        
-        var cmd = new IdCommand();
         cmd.pomPath = pomPath;
         cmd.id = projectId;
         cmd.as = "pom";
         cmd.run();
         assertEquals("pom com.example:my-app:0.0.1-SNAPSHOT", cmd.readProjectId());
+    }
+
+    private IdCommand newUnnamed() {
+        var pomPath = fs.getPath("pom.xml");
+        var projectId = "my-app";
+
+        cmd.pomPath = pomPath;
+        cmd.id = projectId;
+        cmd.run();
+
+        return cmd;
     }
 
     @Test
@@ -92,7 +103,7 @@ class IdCommandTest {
 
     @AfterEach
     void cleanup() throws IOException {
-        Files.walk(tempDir).map(Path::toFile).forEach(File::delete);
+        fs.close();
     }
 
 }
