01e96306be849e0f772cf847e136b6bb84d5987c
andirady <andirady@kendra-solutions.com>
2022-01-25 22:33:47 +0800

    Automatically add <parent> if pom.xml with type pom is found in one of the ancestor dirs

--- src/main/java/com/github/andirady/pomcli/IdCommand.java
+++ src/main/java/com/github/andirady/pomcli/IdCommand.java
@@ -3,9 +3,12 @@ package com.github.andirady.pomcli;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import java.util.Optional;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Parent;
 import org.apache.maven.model.io.DefaultModelReader;
 import org.apache.maven.model.io.DefaultModelWriter;
+import org.apache.maven.model.io.ModelReader;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.ExecutionException;
 import picocli.CommandLine.Model.CommandSpec;
@@ -37,32 +40,71 @@ public class IdCommand implements Runnable {
 
             System.out.println(readProjectId());
         } catch (Exception e) {
-            throw new ExecutionException(spec.commandLine(), "Fail to read " + pomPath + ": " + e.getMessage());
+            throw new RuntimeException("Fail to read " + pomPath + ": " + e.getMessage(), e);
         }
     }
 
     String readProjectId() throws Exception {
         var pomReader = new DefaultModelReader();
-        var pom = pomReader.read(pomPath.toFile(), null);
-        return pom.getPackaging() + " " + pom.getGroupId() + ":" + pom.getArtifactId() + ":" + pom.getVersion();
+        Model pom;
+        try (var is = Files.newInputStream(pomPath)) {
+            pom = pomReader.read(is, null);
+        }
+        var g = pom.getGroupId();
+        var v = pom.getVersion();
+        var parent = pom.getParent();
+        if (parent != null) {
+            if (g == null) {
+                g = parent.getGroupId();
+            }
+            if (v == null) {
+                v = parent.getVersion();
+            }
+        }
+        return pom.getPackaging() + " " + g + ":" + pom.getArtifactId() + ":" + v;
     }
 
     private void updatePom() throws Exception {
-        var pom = new Model();
+        Model pom;
+        var reader = new DefaultModelReader();
+        if (Files.exists(pomPath)) {
+            try (var is = Files.newInputStream(pomPath)) {
+                pom = reader.read(is, null);
+            }
+        } else {
+            pom = new Model();
+            pom.setModelVersion("4.0.0");
+            var parentPom = findParentPom(new DefaultModelReader());
+            if (parentPom != null) {
+                var parent = new Parent();
+                parent.setGroupId(parentPom.model().getGroupId());
+                parent.setArtifactId(parentPom.model().getArtifactId());
+                parent.setVersion(parentPom.model().getVersion());
+                parent.setRelativePath(pomPath.relativize(parentPom.path().getParent()).toString());
+                pom.setParent(parent);
+            }
+        }
+
         var parts = id.split(":", 3);
         
         if (parts.length >= 2) {
             pom.setGroupId(parts[0]);
             pom.setArtifactId(parts[1]);
         } else if (parts.length == 1) {
-            pom.setGroupId("unnamed");
             pom.setArtifactId(parts[0]);
         }
 
         if (parts.length >= 3) {
             pom.setVersion(parts[2]);
-        } else {
-            pom.setVersion("0.0.1-SNAPSHOT");
+        }
+
+        if (pom.getParent() == null) {
+            if (pom.getGroupId() == null) {
+                pom.setGroupId("unnamed");
+            }
+            if (pom.getVersion() == null) {
+                pom.setVersion("0.0.1-SNAPSHOT");
+            }
         }
 
         if (as != null) {
@@ -70,7 +112,38 @@ public class IdCommand implements Runnable {
         }
 
         var pomWriter = new DefaultModelWriter();
-        pomWriter.write(pomPath.toFile(), null, pom);
+        try (var os = Files.newOutputStream(pomPath)) {
+            pomWriter.write(os, null, pom);
+        }
+    }
+
+    record ParentPom(Path path, Model model) {
+    }
+
+    private ParentPom findParentPom(ModelReader pomReader) throws Exception {
+        var parent = pomPath.getParent();
+        // To facilitate test:
+        if (parent == null) {
+            return null;
+        }
+        // Try to find pom.xml with packaging 'pom' up until 5 ancestor folders.
+        for (var i = 0; i < 5; i++) {
+            parent = parent.getParent();
+            if (parent == null) {
+                return null;
+            }
+            var parentPomPath = parent.resolve("pom.xml");
+            if (Files.exists(parentPomPath)) {
+                try (var is = Files.newInputStream(parentPomPath)) {
+                    var pom = pomReader.read(is, null);
+                    if ("pom".equals(pom.getPackaging())) {
+                        return new ParentPom(parentPomPath, pom);
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 
 }
--- src/test/java/com/github/andirady/pomcli/IdCommandTest.java
+++ src/test/java/com/github/andirady/pomcli/IdCommandTest.java
@@ -1,13 +1,13 @@
 package com.github.andirady.pomcli;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.FileSystem;
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,20 +15,18 @@ import org.junit.jupiter.api.Test;
 
 import com.google.common.jimfs.Jimfs;
 
-import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine;
 
 class IdCommandTest {
 
 
     IdCommand cmd;
-    CommandSpec spec;
     FileSystem fs;
 
     @BeforeEach
     void setup() throws IOException {
         fs = Jimfs.newFileSystem();
         cmd = new IdCommand();
-        spec = CommandSpec.forAnnotatedObject(cmd);
     }
 
     @Test
@@ -40,6 +38,19 @@ class IdCommandTest {
         cmd.id = projectId;
         cmd.run();
         assertTrue(Files.exists(cmd.pomPath));
+    }
+
+    @Test
+    void shouldSetModelVersionIfCreatingNewPom() throws IOException {
+        var pomPath = fs.getPath("pom.xml");
+        var projectId = "com.example:my-app:0.0.1";
+
+        cmd.pomPath = pomPath;
+        cmd.id = projectId;
+        cmd.run();
+
+        var s = Files.readString(pomPath);
+        assertTrue(s.contains("<modelVersion>4.0.0</modelVersion>"));
     }
 
     @Test
@@ -86,6 +97,36 @@ class IdCommandTest {
         assertEquals("pom com.example:my-app:0.0.1-SNAPSHOT", cmd.readProjectId());
     }
 
+    @Test
+    void shouldAddParentIfAPomProjectIsFoundAboveTheDirectoryTree() throws Exception {
+        var aPath = fs.getPath("a");
+        var bPath = aPath.resolve("b");
+        var cPath = bPath.resolve("c");
+        var parentPomPath = aPath.resolve("pom.xml");
+        var pomPath = cPath.resolve("pom.xml");
+
+        Files.createDirectory(aPath);
+        Files.createDirectory(bPath);
+        Files.createDirectory(cPath);
+
+        cmd.pomPath = parentPomPath;
+        cmd.id = "a:a:1";
+        cmd.as = "pom";
+        cmd.run();
+
+        cmd.pomPath = pomPath;
+        cmd.id = "c";
+        cmd.as = "jar";
+        cmd.run();
+
+        var pat = Pattern.compile(".*<parent>\\s*<groupId>a</groupId>\\s*<artifactId>a</artifactId>\\s*<version>1</version>\\s*<relativePath>../../..</relativePath>\\s*</parent>", Pattern.MULTILINE);
+        var s = Files.readString(pomPath);
+        var matcher = pat.matcher(s);
+        assertNotNull(matcher);
+        assertTrue(matcher.find());
+        assertEquals("jar a:c:1", cmd.readProjectId());
+    }
+
     private IdCommand newUnnamed() {
         var pomPath = fs.getPath("pom.xml");
         var projectId = "my-app";
@@ -95,10 +136,6 @@ class IdCommandTest {
         cmd.run();
 
         return cmd;
-    }
-
-    @Test
-    void shouldAddParentIfAPomProjectIsFoundAboveTheDirectoryTree() throws Exception {
     }
 
     @AfterEach
