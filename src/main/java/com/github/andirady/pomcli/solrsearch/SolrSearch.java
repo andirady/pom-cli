643d3773839057c4fec7956ec01df99f03ec68fa
Andi Rady Kurniawan <kurniasan@yahoo.com.my>
2023-06-08 13:31:27 +0800

    Convert tabs to whitespaces

--- src/main/java/com/github/andirady/pomcli/solrsearch/SolrSearch.java
+++ src/main/java/com/github/andirady/pomcli/solrsearch/SolrSearch.java
@@ -23,67 +23,68 @@ import java.util.zip.GZIPInputStream;
 
 public class SolrSearch {
 
-	private static final ObjectMapper OM = JsonMapper.builder().addModule(new AfterburnerModule()).build();
-	private static final HttpClient httpClient = HttpClient.newHttpClient();
+    private static final ObjectMapper OM = JsonMapper.builder().addModule(new AfterburnerModule()).build();
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
 
-	public SolrSearchResult search(SolrSearchRequest req) {
-		var httpReq = HttpRequest.newBuilder(makeUri(req)).GET()
-				.headers("Accept", "application/json", "Accept-Encoding", "gzip").build();
-		try {
-			var httpResp = httpClient.send(httpReq, this::gzipBodyHandler);
-			return httpResp.body().get();
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			return null;
-		}
-	}
+    public SolrSearchResult search(SolrSearchRequest req) {
+        var httpReq = HttpRequest.newBuilder(makeUri(req)).GET()
+                .headers("Accept", "application/json", "Accept-Encoding", "gzip").build();
+        try {
+            var httpResp = httpClient.send(httpReq, this::gzipBodyHandler);
+            return httpResp.body().get();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        }
+    }
 
-	private BodySubscriber<java.util.function.Supplier<SolrSearchResult>> gzipBodyHandler(HttpResponse.ResponseInfo respInfo) {
-		if (respInfo.statusCode() != 200) {
-            throw new IllegalStateException("Server returns error: statusCode=" + respInfo.statusCode());
+    private BodySubscriber<java.util.function.Supplier<SolrSearchResult>> gzipBodyHandler(HttpResponse.ResponseInfo respInfo) {
+        var sc = respInfo.statusCode();
+        if (sc != 200) {
+            throw new IllegalStateException("Server returns error: statusCode=" + sc);
         }
 
-		var contEnc = respInfo.headers().firstValue("Content-Encoding");
+        var contEnc = respInfo.headers().firstValue("Content-Encoding");
         if (!contEnc.filter("gzip"::equals).isPresent()) {
-			throw new IllegalStateException("Server returns unexpected encoding: " + contEnc.orElse(""));
-		}
+            throw new IllegalStateException("Server returns unexpected encoding: " + contEnc.orElse(""));
+        }
 
         var upstream = HttpResponse.BodySubscribers.ofInputStream();
-		return BodySubscribers.mapping(upstream, is -> () -> {
-			try (
+        return BodySubscribers.mapping(upstream, is -> () -> {
+            try (
                 var stream = is;
                 var gis = new GZIPInputStream(stream);
             ) {
-				return OM.readValue(gis, SolrSearchResult.class);
-			} catch (IOException e) {
-				throw new UncheckedIOException(e);
-			}
-		});
-	}
+                return OM.readValue(gis, SolrSearchResult.class);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        });
+    }
 
-	private URI makeUri(SolrSearchRequest req) {
-		try {
-			var query = Arrays.stream(SolrSearchRequest.class.getRecordComponents()).map(toKeyValue(req))
-					.filter(Objects::nonNull).map(Object::toString).collect(Collectors.joining("&"));
-			return new URI("https", "search.maven.org", "/solrsearch/select", query, null);
-		} catch (URISyntaxException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+    private URI makeUri(SolrSearchRequest req) {
+        try {
+            var query = Arrays.stream(SolrSearchRequest.class.getRecordComponents()).map(toKeyValue(req))
+                    .filter(Objects::nonNull).map(Object::toString).collect(Collectors.joining("&"));
+            return new URI("https", "search.maven.org", "/solrsearch/select", query, null);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
-	private Function<RecordComponent, String> toKeyValue(SolrSearchRequest req) {
-		return c -> {
-			try {
-				var value = c.getAccessor().invoke(req);
-				if (value == null) {
-					return null;
-				}
-				return c.getName() + "=" + value;
-			} catch (IllegalAccessException | InvocationTargetException e) {
-				throw new UnsupportedOperationException(e);
-			}
-		};
-	}
+    private Function<RecordComponent, String> toKeyValue(SolrSearchRequest req) {
+        return c -> {
+            try {
+                var value = c.getAccessor().invoke(req);
+                if (value == null) {
+                    return null;
+                }
+                return c.getName() + "=" + value;
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new UnsupportedOperationException(e);
+            }
+        };
+    }
 }
