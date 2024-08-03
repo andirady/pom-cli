1f657140132d58c3f59de08661333436c4a81312
Andi Rady Kurniawan <kurniasan@yahoo.com.my>
2024-08-03 12:44:35 +0800

    Add license headers

--- src/test/java/com/github/andirady/pomcli/GetLatestVersionTest.java
+++ src/test/java/com/github/andirady/pomcli/GetLatestVersionTest.java
@@ -1,9 +1,23 @@
+/**
+ * Copyright 2021-2024 Andi Rady Kurniawan
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.andirady.pomcli;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
@@ -11,7 +25,6 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandler;
 import java.time.Instant;
 
 import org.junit.jupiter.api.Test;
