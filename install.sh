ffe5667d3dade86ccf3cd57a53f8bdc9b2be851a
Andi Rady Kurniawan <kurniasan@yahoo.com.my>
2025-08-15 01:56:58 +0800

    Use `PREFIX` and reduce verbosity (#11)

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 pwd=$PWD
 workdir=$HOME/.cache/pom-cli
 
+if [ -z "$PREFIX" ]; then
+    if [[ $EUID -eq 0 ]]; then
+        PREFIX=/usr/local
+    else
+        PREFIX=$HOME/.local
+    fi
+fi
+
 mkdir -p $workdir
 cd $workdir
 
@@ -8,16 +16,17 @@ v=0.9.4
 dist_name="pomcli-${v}-linux-x86_64"
 archive_name="${dist_name}.tar.gz"
 
-mkdir -p $HOME/.local/share/pom-cli
+mkdir -p $PREFIX/share/pom-cli
 
-curl -v -O -L "https://github.com/andirady/pom-cli/releases/download/v${v}/${archive_name}"
+curl -sO -L "https://github.com/andirady/pom-cli/releases/download/v${v}/${archive_name}"
 if [ ! -f $archive_name ]; then
     echo "Download fail" >&2
     exit 1
 fi
 
-tar xzf $archive_name -C $HOME/.local/bin/ $dist_name/bin/pom --strip-components=2
-tar xzf $archive_name -C $HOME/.local/share/pom-cli/ $dist_name/LICENSE.txt --strip-components=1
+mkdir -p $prefix/bin
+tar xzf $archive_name -C $PREFIX/bin/ $dist_name/bin/pom --strip-components=2
+tar xzf $archive_name -C $PREFIX/share/pom-cli/ $dist_name/LICENSE.txt --strip-components=1
 
 cd $pwd
 rm -rf $workdir
