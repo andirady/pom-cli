8f053566d69bd3a50a842a3eba064f6f58e7d0f6
Andi Rady Kurniawan <kurniasan@yahoo.com.my>
2025-08-21 00:26:49 +0800

    refractor: move MavenRepositorySystemSupplier under the impl package

--- src/main/java/com/github/andirady/pomcli/impl/MavenRepositorySystemSupplier.java
+++ src/main/java/com/github/andirady/pomcli/impl/MavenRepositorySystemSupplier.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.andirady.pomcli.aether;
+package com.github.andirady.pomcli.impl;
 
 import java.util.Map;
 
