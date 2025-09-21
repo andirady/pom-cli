7263a79a33b167efd1802515fa71d7b1df6552cd
Andi Rady Kurniawan <kurniasan@yahoo.com.my>
2025-09-21 12:21:38 +0800

    feat(id): accept directory or jar for pom path

--- src/test/java/com/github/andirady/pomcli/IdCommandTest.java
+++ src/test/java/com/github/andirady/pomcli/IdCommandTest.java
@@ -334,6 +334,38 @@ class IdCommandTest extends BaseTest {
         assertSame(0, ec);
     }
 
+    @Test
+    void shouldAcceptJarAsPomPath() throws IOException {
+        var pomPath = projectPath.resolve("example.jar");
+
+        try (var os = Files.newOutputStream(pomPath);
+                var jar = new java.util.jar.JarOutputStream(os)) {
+            var entry = new java.util.jar.JarEntry("META-INF/maven/g/a/pom.xml");
+            jar.putNextEntry(entry);
+            jar.write("""
+                    <project xmlns="http://maven.apache.org/POM/4.0.0"
+                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                      xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                      <modelVersion>4.0.0</modelVersion>
+                      <groupId>g</groupId>
+                      <artifactId>a</artifactId>
+                      <version>v</version>
+                    </project>
+                    """.getBytes());
+            jar.closeEntry();
+        }
+
+        var out = new StringWriter();
+        var underTest = new CommandLine(new Main());
+        underTest.setOut(new PrintWriter(out));
+
+        var ec = underTest.execute("id", "-f", pomPath.toString());
+        var actual = out.toString();
+
+        assertSame(0, ec);
+        assertEquals("jar g:a:v%n".formatted(), actual);
+    }
+
     @AfterEach
     void cleanup() throws IOException {
         deleteRecursive(projectPath);
